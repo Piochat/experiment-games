f109bdb20755fc5470e2f8294983e3106d91e2ca
Koke Gudiel <kokegudiel2@gmail.com>
2022-06-24 10:38:01 -0600

    layout

---
+++ src/components/navbar.svelte
@@ -0,0 +1,13 @@
+<nav class="bg-gray-800 py-4 relative">
+	<div class="container mx-auto flex">
+		<img
+			class="h-10"
+			src="https://static.wikia.nocookie.net/logopedia/images/f/f6/PCEngine_logo.svg"
+			alt="logaso"
+		/>
+		<div class="container">
+			<a class="text-white text-2xl" href="/">Darling</a>
+			<a class="text-white text-2xl" href="/">Piso</a>
+		</div>
+	</div>
+</nav>
--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,5 +1,9 @@
 <script>
-  import "../app.css";
+	import Navbar from "../components/navbar.svelte";
+
+	import "../app.css";
 </script>
+
+<Navbar />
 
 <slot />
